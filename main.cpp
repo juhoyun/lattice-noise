59ee3d5ffd48ed333f471f44d0c5f04b664994b6
Jay <juho.yun@gmail.com>
2015-09-19 12:27:22 -0700

    in progress

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include <stdio.h>
+#include <math.h>
 #include "PseudoRandom.h"
 
 float heightmap[512][512];
 
 using namespace GenericLatticeNoiseAlgorithm;
 
+typedef float (*ProximityFunction)(PseudoRandom& pRnd, float x, float y, float lambda, float xHash, float yHash);
+typedef float (*FadeFunction)(float x, float y);
+
+float Gradient(PseudoRandom& pRnd, float x, float y, float lambda, float xHash, float yHash)
+{
+    return (x * pRnd.HashRandom(xHash, yHash, 0) + y * pRnd.HashRandom(xHash, yHash, 1)) / (lambda);
+}
+
+float Hermite(float x, float y)
+{
+    return (1 - (x * x * (3 - 2 * x))) * (1 - (y * y * (3 - 2 * y)));
+}
+
 int main()
 {
 	PseudoRandom pRnd;
 	int size = 512;
+	int numberOfOctaves = 3;
+    int initialStep = size / 4;
+    FadeFunction Fade = Hermite;
+    ProximityFunction Proximity = Gradient;
 	pRnd.SetScale(1024.f / size);
 
 	for (int x = 0; x < size + 1; x += 1)
@@ -18,20 +36,22 @@ int main()
 			heightmap[x][y] = 0.5f;
 		}
 	}
-#if 0
+
 	for (int octave = 0; octave < numberOfOctaves; octave++)
 	{
 		int sizeGrid;
 		float sizeGridF;
 		int init = 0;
-		sizeGrid = initialStep / (int)Math.Pow(2.0, octave);
-		if (rb_G01.IsChecked == true) { init = 0; }
-		if (rb_G02.IsChecked == true) { init = - sizeGrid / 2; }
-		if (rb_G03.IsChecked == true) { init = - sizeGrid / 4; }
+		sizeGrid = initialStep / (1 << (octave));
+		init = 0;
+		//if (rb_G01.IsChecked == true) { init = 0; }
+		//if (rb_G02.IsChecked == true) { init = - sizeGrid / 2; }
+		//if (rb_G03.IsChecked == true) { init = - sizeGrid / 4; }
 
 		sizeGridF = (float)sizeGrid;
 		float relativeSize = sizeGridF / size;
-		float persistence = (float)Math.Pow(relativeSize, 1.0 - Persistence_slider.Value);
+		//float persistence = (float)pow(relativeSize, 1.0 - Persistence_slider.Value);
+		float persistence = relativeSize;
 
 		int gridU, gridV;
 		float hBase;                          
@@ -40,7 +60,13 @@ int main()
 		{
 			for (int x = init; x < size + 1; x += sizeGrid)
 			{
-				if (x == size - sizeGrid) { gridU = sizeGrid + 1; } else { gridU = sizeGrid; }
+				if (x == size - sizeGrid)
+				{
+					gridU = sizeGrid + 1;
+				} else 
+				{ 
+					gridU = sizeGrid; 
+				}
 				for (int y = init; y < size + 1; y += sizeGrid)
 				{
 					if (y == size - sizeGrid) { gridV = sizeGrid + 1; } else { gridV = sizeGrid; }
@@ -53,17 +79,18 @@ int main()
 							{
 								float us = u / sizeGridF;
 								float vs = v / sizeGridF;
+
 								hBase =
-											Proximity(u, v, sizeGridF, (float)x, (float)y)
+											Proximity(pRnd, u, v, sizeGridF, (float)x, (float)y)
 											* Fade(u / sizeGridF, v / sizeGridF)
 
-											+ Proximity(u - sizeGridF, v, sizeGridF, (float)x + sizeGridF, (float)y)
+											+ Proximity(pRnd, u - sizeGridF, v, sizeGridF, (float)x + sizeGridF, (float)y)
 											* Fade(1 - u / sizeGridF, v / sizeGridF)
 
-											+ Proximity(u, v - sizeGridF, sizeGridF, (float)x, (float)y + sizeGridF)
+											+ Proximity(pRnd, u, v - sizeGridF, sizeGridF, (float)x, (float)y + sizeGridF)
 											* Fade(u / sizeGridF, 1 - v / sizeGridF)
 
-											+ Proximity(u - sizeGridF, v - sizeGridF, sizeGridF, (float)x + sizeGridF, (float)y + sizeGridF)
+											+ Proximity(pRnd, u - sizeGridF, v - sizeGridF, sizeGridF, (float)x + sizeGridF, (float)y + sizeGridF)
 											* Fade(1 - u / sizeGridF, 1 - v / sizeGridF);
 
 								heightmap[x + (int)u][y + (int)v] += hBase * persistence;
@@ -74,6 +101,6 @@ int main()
 			}
 		}
 	}
-#endif
+
 	return 0;
 }
